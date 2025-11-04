d9251415924f23efa910b0f237fdbc2be463dfcc
khoi <khoivma4709@ut.edu.vn>
2025-11-04 17:17:00 +0700

    initialize owner module

---
+++ apps/owner-backend-spring/src/main/java/com/ccm/OwnerBackendSpring.java
@@ -0,0 +1,13 @@
+package com.ccm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerBackendSpring
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OwnerBackendSpring.class, args);
+    }
+}
